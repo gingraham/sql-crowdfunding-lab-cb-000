1ac85d7625094009732649cf407d0db7a2a5b1b2
Gerald Ingraham Ingraham <gingraham@icloud.com>
2017-09-26 06:06:14 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,3 @@
+CREATE TABLE projects(id INTEGER PRIMARY KEY,title,category, funding_goal, start_date, end_date);
+CREATE TABLE users(id INTEGER PRIMARY KEY,name, age);
+CREATE TABLE pledges(id INTEGER PRIMARY KEY, user_id, project_id, amount);
